3412413949ce44516b2f792262b19efa592fc6a2
unknown <wasif22001@gmail.com>
2024-08-12 23:35:27 +0500

    Created an initial post endpoint

--- app.js
+++ app.js
@@ -35,6 +35,19 @@ app.get('/car/:id',function(req,res){
     res.end();
 })
 
+app.post('/car/:car',function(req,res){
+    
+    const id = req.params.id;
+
+    if (id >= 0 && id < cars.length) {
+        res.write(cars[id]);
+    }
+    else{
+        res.write("Not Found!");
+    }
+    res.end();
+})
+
 app.listen(9000, function(req,res){
     console.log("Cars server is Running.......")
 });
